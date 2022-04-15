1868bbd6440479780a41f651aaf2b4089d8a9c4b
LAPTOP-56J4U8VG\lydia <xuanwen1101@outlook.com>
2022-04-14 22:58:38 -0400

    Add JavaScript Validation function.

--- SchoolProject_W2022/Scripts/teachers.js
+++ SchoolProject_W2022/Scripts/teachers.js
@@ -1,5 +1,7 @@
 ﻿function addTeacher() {
 
+	if (!validateTeacher()) return;
+
 	//goal: send a request with POST data of teacherfname, teacherlname, employeenumber, hiredate, and salary.
 	//POST : http://localhost:56815/api/TeacherData/addTeacher
 
@@ -50,6 +52,8 @@
 
 function updateTeacher(TeacherId) {
 
+	if (!validateTeacher()) return;
+
 	//goal: send a request with POST data of teacherfname, teacherlname, employeenumber, hiredate, and salary.
 	//POST : http://localhost:56815/api/TeacherData/UpdateTeacher/{id}
 
@@ -88,4 +92,59 @@ function updateTeacher(TeacherId) {
 	//POST information sent through the .send() method
 	rq.send(JSON.stringify(TeacherData));
 
+}
+
+function validateTeacher() {
+
+	var IsValid = true;
+	var ErrorMsg = "";
+	var ErrorBox = document.getElementById("ErrorBox");
+	var TeacherFname = document.getElementById('TeacherFname').value;
+	var TeacherLname = document.getElementById('TeacherLname').value;
+	var EmployeeNumber = document.getElementById('EmployeeNumber').value;
+	var TeacherSalary = document.getElementById('TeacherSalary').value;
+
+	//First Name is one or more characters
+	if (TeacherFname.length < 1) {
+		IsValid = false;
+		ErrorMsg += "First Name must be one or more characters.<br>";
+	}
+	//Last Name is one or more characters
+	if (TeacherLname.length < 1) {
+		IsValid = false;
+		ErrorMsg += "Last Name must be one or more characters.<br>";
+	}
+	//EmployeeNumber is valid pattern
+	if (!validateEmployeeNumber(EmployeeNumber)) {
+		IsValid = false;
+		ErrorMsg += "Employee Number must start with T and follow with three numbers.<br>";
+	}
+
+	//TeacherSalary
+	if (!validateSalary(TeacherSalary)) {
+		IsValid = false;
+		ErrorMsg += "Please enter a valid Salary.<br>";
+	}
+
+	if (!IsValid) {
+		ErrorBox.style.display = "block";
+		ErrorBox.style.color = "red";
+		ErrorBox.innerHTML = ErrorMsg;
+	} else {
+		ErrorBox.style.display = "none";
+		ErrorBox.innerHTML = "";
+	}
+
+
+	return IsValid;
+}
+
+function validateEmployeeNumber(idValue) {
+	var idPattern = /^[tT][0-9]{3}$/;
+	return idPattern.test(idValue);
+}
+
+function validateSalary(salary) {
+	var salaryPattern = /^[0-9]*.[0-9]{0,2}$/;
+	return salaryPattern.test(salary);
 }
